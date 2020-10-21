4e8534a5c8d6bcd2d0977fb6a27e55cf4d690e3d
MARIE_M <fifi.maresova@gmail.com>
2020-10-21 12:31:08 +0200

    Event listener working for both players on one selected field

--- hra.js
+++ hra.js
@@ -10,24 +10,17 @@ třetí na spojení obou výše - tu spustí kliknutí
 - bonus (disable a animace)
 */
 
-/*
-const switchPlayer = () => {
-  const witchPlayer = document.getElementById('player');
-  witchPlayer.classList.toggle('playerCross');
-};
-
-const btnElm = document.querySelector('.btn1');
-btnElm.addEventListener('click', switchPlayer);
-*/
-
-const switchPlayer = (event) => {
-  event.target.classList.toggle('playerCross');
-};
-
-document.querySelector('.btn1').addEventListener('click', switchPlayer);
+/* kolečko na první pole */
 
 const circlePlayed = (event) => {
   event.target.classList.add('selectedCircle');
 };
 
 document.querySelector('#btn1').addEventListener('click', circlePlayed);
+
+/* křížek na druhé pole */
+const crossPlayed = (event) => {
+  event.target.classList.add('selectedCross');
+};
+
+document.querySelector('#btn2').addEventListener('click', crossPlayed);
