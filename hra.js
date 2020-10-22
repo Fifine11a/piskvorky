3af29dc4651fbf028b34a8f21b4fd65fd450123a
MARIE_M <fifi.maresova@gmail.com>
2020-10-22 19:12:25 +0200

    Different approach, same result...

--- hra.js
+++ hra.js
@@ -4,7 +4,21 @@ console.log('You better work, b*tch!');
 
 let player = 'circle';
 
-if (player === 'circle') {
+if (document.getElementById('player').classList.contains('.playerCross')) {
+  const crossPlayed = (event) => {
+    event.target.classList.add('selectedCross');
+  };
+
+  const buttons = document.querySelectorAll('button');
+
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', crossPlayed);
+  }
+
+  document.getElementById('player').classList.toggle('.playerCross');
+
+  player = 'circle';
+} else {
   const circlePlayed = (event) => {
     event.target.classList.add('selectedCircle');
   };
@@ -14,20 +28,7 @@ if (player === 'circle') {
     buttons[i].addEventListener('click', circlePlayed);
   }
 
+  document.getElementById('player').classList.toggle('.playerCross');
+
   player = 'cross';
-} else {
-  document.getElementById('player').classList.toggle('.playerCross');
-
-  const crossPlayed = (event) => {
-    event.target.classList.add('selectedCross');
-  };
-
-  const buttons = document.querySelectorAll('button');
-  for (let i = 0; i < buttons.length; i += 1) {
-    buttons[i].addEventListener('click', crossPlayed);
-  }
-
-  document.getElementById('player').classList.toggle('.playerCross');
-
-  player = 'circle';
 }
