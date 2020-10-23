be51ff69bcc0eb908847fa30ddbd3b527377e88b
MARIE_M <fifi.maresova@gmail.com>
2020-10-23 17:35:17 +0200

    Pseudo elements aligned

--- hra.js
+++ hra.js
@@ -9,11 +9,13 @@ const player = document.querySelector('.player');
 const gamePlayed = (event) => {
   if (whoPlays === 'circle') {
     event.target.classList.add('selectedCircle');
+    event.target.setAttribute('disabled', true);
     player.className = 'player playerCross';
     whoPlays = 'cross';
   } else {
     event.target.classList.add('selectedCross');
     player.className = 'player playerCircle';
+    event.target.setAttribute('disabled', true);
     whoPlays = 'circle';
   }
 };
